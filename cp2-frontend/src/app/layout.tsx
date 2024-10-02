b20d68e92a6aa80462e334f6599af58c0766a75a
Ribaas <ribasleonardo92@gmail.com>
2024-10-01 22:04:14 -0300

    Movendo pasta components para o local correto

--- cp2-frontend/src/app/layout.tsx
+++ cp2-frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Cabecalho from "./components/Cabecalho/Cabecalho";
-import Rodape from "./components/Rodape/Rodape";
+import Cabecalho from "../components/Cabecalho/Cabecalho";
+import Rodape from "../components/Rodape/Rodape";
 
 export const metadata: Metadata = {
   title: "Worlds in Collision",
