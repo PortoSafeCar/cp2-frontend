f4cc1cb052b524c7dbf4dcdcdaaea975f0cf3bf0
heitxrr <heitorrhh@hotmail.com>
2024-10-01 23:41:52 -0300

    formatando documentos

--- cp2-frontend/src/app/recepcao/page.tsx
+++ cp2-frontend/src/app/recepcao/page.tsx
@@ -11,9 +11,18 @@ export default function Recepcao() {
         alt="Imagem da NASA do dia"
       />
       <p>
-      O lançamento de Worlds in Collision gerou intensa controvérsia, especialmente dentro da comunidade científica. Astrônomos e físicos criticaram ferozmente as ideias de Velikovsky, considerando-as pseudociência por falta de evidências empíricas sólidas. Mesmo assim, o livro foi um sucesso de vendas, capturando a imaginação do público com sua mistura intrigante de ciência, mitologia e história.
-
-Apesar das críticas, Worlds in Collision teve um impacto duradouro, inspirando debates sobre a natureza dos cataclismos antigos e influenciando movimentos alternativos na ciência e na arqueologia. As ideias de Velikovsky também estimularam a criação de outras teorias catastrofistas e continuam a ser estudadas e debatidas em círculos acadêmicos e amadores.
+        O lançamento de Worlds in Collision gerou intensa controvérsia,
+        especialmente dentro da comunidade científica. Astrônomos e físicos
+        criticaram ferozmente as ideias de Velikovsky, considerando-as
+        pseudociência por falta de evidências empíricas sólidas. Mesmo assim, o
+        livro foi um sucesso de vendas, capturando a imaginação do público com
+        sua mistura intrigante de ciência, mitologia e história. Apesar das
+        críticas, Worlds in Collision teve um impacto duradouro, inspirando
+        debates sobre a natureza dos cataclismos antigos e influenciando
+        movimentos alternativos na ciência e na arqueologia. As ideias de
+        Velikovsky também estimularam a criação de outras teorias catastrofistas
+        e continuam a ser estudadas e debatidas em círculos acadêmicos e
+        amadores.
       </p>
     </>
   );
