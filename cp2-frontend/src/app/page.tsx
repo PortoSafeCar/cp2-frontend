a78cf4e2aa9018e22db8d8c1505f3914a8958b7e
heitxrr <heitorrhh@hotmail.com>
2024-10-01 23:42:48 -0300

    formatando page

--- cp2-frontend/src/app/page.tsx
+++ cp2-frontend/src/app/page.tsx
@@ -11,14 +11,24 @@ export default function Home() {
         alt="Imagem da NASA do dia"
       />
       <p>
-      Worlds in Collision é uma obra revolucionária escrita pelo psicanalista e cientista russo Immanuel Velikovsky, publicada em 1950. O livro desafia as concepções tradicionais sobre história, astronomia e geologia, propondo uma série de eventos cósmicos que teriam moldado a história da Terra e da humanidade. A premissa central de Velikovsky é que catástrofes de magnitude planetária ocorreram na antiguidade devido a interações violentas entre planetas, principalmente Vênus e Marte, o que teria causado profundas mudanças na Terra.
-
+        Worlds in Collision é uma obra revolucionária escrita pelo psicanalista
+        e cientista russo Immanuel Velikovsky, publicada em 1950. O livro
+        desafia as concepções tradicionais sobre história, astronomia e
+        geologia, propondo uma série de eventos cósmicos que teriam moldado a
+        história da Terra e da humanidade. A premissa central de Velikovsky é
+        que catástrofes de magnitude planetária ocorreram na antiguidade devido
+        a interações violentas entre planetas, principalmente Vênus e Marte, o
+        que teria causado profundas mudanças na Terra.
       </p>
       <p>
-      Worlds in Collision é uma obra que provoca reflexões profundas sobre o nosso entendimento do universo e do passado da Terra. Embora amplamente desacreditada pela ciência convencional, a teoria de Velikovsky sobre catástrofes cósmicas em tempos históricos continua a fascinar leitores e a estimular debates sobre a relação entre mitologia, história e ciência.
-
-O livro se destaca como uma peça importante na literatura que explora eventos cósmicos e suas possíveis implicações na história humana, desafiando as fronteiras entre ciência e especulação.
-
+        Worlds in Collision é uma obra que provoca reflexões profundas sobre o
+        nosso entendimento do universo e do passado da Terra. Embora amplamente
+        desacreditada pela ciência convencional, a teoria de Velikovsky sobre
+        catástrofes cósmicas em tempos históricos continua a fascinar leitores e
+        a estimular debates sobre a relação entre mitologia, história e ciência.
+        O livro se destaca como uma peça importante na literatura que explora
+        eventos cósmicos e suas possíveis implicações na história humana,
+        desafiando as fronteiras entre ciência e especulação.
       </p>
     </>
   );
