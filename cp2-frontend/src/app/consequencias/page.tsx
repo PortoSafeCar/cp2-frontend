37b0444c2e2358ca9cfef3e39153ca4d3ab9f6e7
heitxrr <heitorrhh@hotmail.com>
2024-10-01 23:42:29 -0300

    formatando documentos: consequencias

--- cp2-frontend/src/app/consequencias/page.tsx
+++ cp2-frontend/src/app/consequencias/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Consequencias() {
   return (
     <>
-      <h2 className="text-2xl font-bold">Principais Consequências e Impactos</h2>
+      <h2 className="text-2xl font-bold">
+        Principais Consequências e Impactos
+      </h2>
       <Image
         src="http://localhost:3000/api/apod/2024-09-27"
         width={400}
@@ -11,10 +13,16 @@ export default function Consequencias() {
         alt="Imagem da NASA do dia"
       />
       <p>
-De acordo com Velikovsky, esses eventos cósmicos resultaram em várias mudanças geológicas, como o deslocamento de massas de água e o levantamento de montanhas. Ele também acredita que influenciaram o clima e até mesmo o comportamento das populações antigas, que teriam vivenciado dias de escuridão, terremotos devastadores e inundações catastróficas.
-
-Uma das afirmações mais ousadas de Velikovsky é a ideia de que a história humana não é linear, mas pontuada por eventos de destruição em grande escala causados por forças extraterrestres. Ele sugere que muitas dessas histórias de destruição foram mitologizadas e incorporadas em textos religiosos e folclóricos ao redor do mundo.
-
+        De acordo com Velikovsky, esses eventos cósmicos resultaram em várias
+        mudanças geológicas, como o deslocamento de massas de água e o
+        levantamento de montanhas. Ele também acredita que influenciaram o clima
+        e até mesmo o comportamento das populações antigas, que teriam
+        vivenciado dias de escuridão, terremotos devastadores e inundações
+        catastróficas. Uma das afirmações mais ousadas de Velikovsky é a ideia
+        de que a história humana não é linear, mas pontuada por eventos de
+        destruição em grande escala causados por forças extraterrestres. Ele
+        sugere que muitas dessas histórias de destruição foram mitologizadas e
+        incorporadas em textos religiosos e folclóricos ao redor do mundo.
       </p>
     </>
   );
