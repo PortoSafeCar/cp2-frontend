41e9338d0fe257bfb8fc39f3861f0c634edd08d4
Ribaas <ribasleonardo92@gmail.com>
2024-10-01 22:24:43 -0300

    Adicionando estrutura das paginas

--- cp2-frontend/src/app/teorias/page.tsx
+++ cp2-frontend/src/app/teorias/page.tsx
@@ -1,3 +1,20 @@
 export default function Teorias() {
-  return <>Teorias</>;
+  return (
+    <main>
+      <h2>Teorias Principais</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta odio iure
+        sapiente, eaque sed eius suscipit quod sit molestiae tenetur quis
+        eligendi voluptatum dolore, unde quae minus perferendis doloribus odit,
+        nobis in ea illo amet incidunt. Natus assumenda debitis libero
+        voluptatum! Unde dolore cum, consectetur reiciendis quibusdam culpa,
+        officia tempore quasi, magnam voluptatem facere. Laboriosam, quas
+        magnam? Dignissimos distinctio, tempora dicta ad porro dolores facilis
+        quos ipsam dolore excepturi, minus error tempore perferendis obcaecati
+        nemo deleniti labore ipsa ex expedita nisi ipsum at odit fuga? Et
+        distinctio, fuga, esse totam nam earum debitis veniam impedit, odio
+        obcaecati inventore ipsa quas!
+      </p>
+    </main>
+  );
 }
